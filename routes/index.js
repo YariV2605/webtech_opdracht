ba7e2c3f952a7821343638b8d49ec82c3579ab70
YariV2605 <115528654+YariV2605@users.noreply.github.com>
2023-11-09 17:12:12 +0100

    homepage linken naar index.html

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
+const {join} = require("path");
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
+  res.sendFile(join(__dirname, '../index.html'))
 });
 
 module.exports = router;
