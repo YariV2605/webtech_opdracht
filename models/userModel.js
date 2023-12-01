a4f696e105ab9d6dbcd7d5a83e41f57e7072612d
YariV2605 <115528654+YariV2605@users.noreply.github.com>
2023-12-01 10:02:01 +0100

    model prep for login

--- models/userModel.js
+++ models/userModel.js
@@ -8,6 +8,10 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     name: String,
     favorieten: [{type: Schema.Types.ObjectId, ref: "bier"}],
+    salt:{
+        type: String,
+        required: true
+    },
     passwordHash: {
         type: String,
         required : true
